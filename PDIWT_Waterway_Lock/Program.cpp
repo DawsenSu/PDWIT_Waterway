0b5e480a9fadd93f1dcd3901ec2e0cd5da5ca906
苏东升 <tjdxsds@outlook.com>
2020-01-03 13:01:03 +0800

    Test my branch

--- PDIWT_Waterway_Lock/Program.cpp
+++ PDIWT_Waterway_Lock/Program.cpp
@@ -14,9 +14,11 @@ void PDIWT::Waterway::Lock::KeyinCommands::CreateDolphinP11(System::String^ unpa
 void PDIWT::Waterway::Lock::KeyinCommands::CreateGuidewallP2(System::String^ unparsed)
 {
 	mdlOutput_message(L"CreateGuidewallP2");
+	mdlOutput_message(L"CreateGuidewallP2");
 
 }
 
 void PDIWT::Waterway::Lock::KeyinCommands::CreateGuidewallP3(System::String^ unparsed)
 {
-	mdlOutput_message(L"CreateGuidewallP3");}
+	mdlOutput_message(L"CreateGuidewallP3");
+}
